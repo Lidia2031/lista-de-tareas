cda8c3c630d86a54407ebf6df1694fc9c6a984b3
Lidia <al222210635@gmail.com>
2025-03-20 02:58:33 -0600

    Cambiar Procfile para usar Gunicorn

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from todor import create_app
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    # Utiliza el puerto desde la variable de entorno PORT o 5000 como predeterminado
+    port = int(os.environ.get("PORT", 5000))
+    # Ejecuta en 0.0.0.0 para que sea accesible desde fuera del entorno local
+    app.run(host='0.0.0.0', port=port)
